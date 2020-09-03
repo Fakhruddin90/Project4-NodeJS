fb308ce0b89d7130d37c6ac59376c4fbc1513016
Fakhruddin90 <Fakhruddin90.com>
2020-09-03 15:07:51 +0800

    commit changes

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "This is test";
@@ -34,8 +35,7 @@ con.connect(function(err) {
   });
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+
 
 // app.get("/", function(req, res){
 //     res.sendFile(__dirname + "/templates/index.ejs");
